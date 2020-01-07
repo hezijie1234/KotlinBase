c2193989f4513ed2a2ec2d13bf0d6f113dff5dde
何子杰 <1710276127@qq.com>
2020-01-07 18:08:06 +0800

    委托属性的值修改监测

---
+++ app/src/main/java/com/zijie/kotlinbase/InlineClass.kt
@@ -0,0 +1,79 @@
+package com.zijie.kotlinbase
+
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
+
+/**
+ * Created by hezijie on 2020/1/7.
+ */
+fun main() {
+    Deliver(ImplBase()).printYou()
+    val e = Example()
+    println(e.p)
+    e.p = "New"
+    println( e.lazyValue)
+    println( e.lazyValue)
+    val u = User()
+    u.name = "first"
+    u.name = "second"
+}
+
+
+
+class User{
+    var name:String by Delegates.observable("name init"){
+        property, oldValue, newValue -> println("$oldValue->$newValue")
+    }
+    var age:Int by Delegates.vetoable(1,{
+            property, oldValue, newValue ->
+        true
+    })
+}
+
+class Delegate{
+    operator fun  getValue(thisRef:Any?,property:KProperty<*>):String{
+        return "$thisRef, thank you for delegating '${property.name}' to me!"
+    }
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String){
+        println("$value has been assigned to '${property.name}' in $thisRef.")
+    }
+
+}
+
+class Example{
+    var p:String by Delegate()
+    val lazyValue:String by lazy(LazyThreadSafetyMode.NONE) {
+        println("computed")
+        "hello"
+    }
+}
+
+interface BaseClass{
+    fun printYou();
+}
+
+class ImplBase:BaseClass{
+    override fun printYou() {
+        println("这是实现类ImplBase")
+    }
+
+}
+
+class Deliver(val clazz: BaseClass):BaseClass by clazz{
+    override fun printYou() {
+        println("这个是${javaClass}")
+    }
+}
+
+inline class UInt(val i:Int){
+
+}
+
+fun  compute(i:Int){
+
+}
+
+fun compute(i:UInt){
+
+}
--- app/src/main/java/com/zijie/kotlinbase/generic/GenericDemo.kt
+++ app/src/main/java/com/zijie/kotlinbase/generic/GenericDemo.kt
@@ -1,5 +1,11 @@
 package com.zijie.kotlinbase.generic
 
+import android.annotation.TargetApi
+import android.os.Build
+import java.io.File
+import java.util.function.BinaryOperator
+import java.util.function.IntBinaryOperator
+
 /**
  * Created by hezijie on 2019/12/30.
  */
@@ -40,6 +46,45 @@ enum class MyColor(val i:Int){
     BLUE(0x0000FF)
 }
 
+enum class ProtocolState{
+    WAITING { override fun signal() = TALKING },
+    TALKING { override fun signal() = WAITING };
+    abstract fun signal(): ProtocolState
+}
+
+@TargetApi(Build.VERSION_CODES.N)
+enum class IntDemo:BinaryOperator<Int>,IntBinaryOperator{
+    PLUS{
+        override fun apply(t: Int, u: Int): Int {
+            return t + u
+        }
+
+    },
+    TIMES{
+        override fun apply(t: Int, u: Int): Int {
+            return  t * u
+        }
+    };
+
+    /**
+     * 枚举类可以统一实现接口中的方法，也可以像上面的匿名内部类一样单独实现
+     */
+    override fun applyAsInt(left: Int, right: Int): Int {
+        return apply(left,right)
+    }
+}
+
+enum class RGB{
+    RED,GREEN,BLUE
+}
+inline fun <reified T:Enum<T>> printAllValues(){
+    println(enumValues<T>().joinToString { it.name })
+}
+
+
+
+
+@TargetApi(Build.VERSION_CODES.N)
 fun main() {
 //    val intArr:Array<Int> = arrayOf(1,2,3)
 //    val any = Array<Any>(3){}
@@ -48,7 +93,86 @@ fun main() {
 //    add(Com())
     val inner:Outer.Inner = Outer.Inner()
     println(inner.num())
+    val  a = 13
+    val b = 31
+    for (f in IntDemo.values()){
+        println("$f($a, $b) = ${f.apply(a, b)}")
+    }
+    println(IntDemo.valueOf("PLUS"))
+    printAllValues<RGB>()
+    val obj = object : A(1),B {
+        override val i = 11
+    }
+
+    //直接创建一个没有任何基类的匿名内部类
+    val obj2  = object {
+
+    }
+    val  g = singleClass.a
+    SC.m = 100;
+    val y = MyCl
+    println(y.javaClass)
+
 }
+
+interface Factory<T>{
+    fun create():T
+}
+//类型别名的使用
+typealias MyCl = MyClassOne
+class MyClassOne{
+
+    companion object:Factory<MyClassOne>{
+
+        override fun create(): MyClassOne {
+            return MyClassOne()
+        }
+
+    }
+
+}
+
+/**
+ * 对象声明
+ */
+object SC:C(){
+    var m = 7;
+}
+
+object singleClass{
+    var a = 10
+}
+
+open class C{
+    private fun priFun() = object {
+            val x :String = "x"
+        }
+
+    fun pubFun()= object {
+            val m:String = "m"
+        }
+
+
+    fun  bar(){
+        var a = priFun().x
+//        val b = pubFun().m
+    }
+}
+
+
+
+open class A(val a:Int){
+    constructor():this(11){
+
+    }
+    open val i = a;
+}
+
+interface B{
+
+}
+
+
 class Com : Comparable<Int>{
     override fun compareTo(other: Int): Int {
         return 1;
